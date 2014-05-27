f5f48a0a7891fb08632902bf7e65fff149fdf3c1
Douglas dos Santos <douglas.santoz@gmail.com>
2014-05-27 01:14:02 -0300

    05-27

--- formmail.php
+++ formmail.php
@@ -1,30 +1,52 @@
-<?
-$nome = $_POST['nome'];
-$email = $_POST['email'];
-$email = $_POST['telefone'];
-$campomensagem = $_POST['mensagem'];
-//aqui nós resgatamos as variáveis do formulário
-$para = "Douglas Santos<douglas.santoz@gmail.com>";
-$assunto = "Mensagem enviada do site";
-$mensagem = "$nome
-$email
-$telefone
-$campomensagem";
-/*aqui foram definidos: o local onde será enviado o e-mail($para)
-o assunto do e-mail enviado ($assunto)
-e a mensagem enviada ($mensagem) que contém todos os campos preenchido no formulário*/
-if (strlen($nome) < 3)
-echo "Digite seu nome!<BR> <a href=\'javascript:history.back(1);\'>Voltar</a>";
-elseif (strlen($email) <= 3)
-echo "E-mail inválido!<BR> <a href=\'javascript:history.back(1);\'>Voltar</a>";
-elseif (strlen($telefone) <= 3)
-echo "Telefone inválido!<BR> <a href=\'javascript:history.back(1);\'>Voltar</a>";
-elseif (strlen($campomensagem) <= 3)
-echo "Escreva uma mensagem!<BR> <a href=\'javascript:history.back(1);\'>Voltar</a>";
-else{
-mail($para,$assunto,$telefone,$mensagem);
-echo "O formulário foi preenchido e enviado com sucesso!!";
+<?php
+if (isset($_POST['btnEnviar'])){
+ 
+        //Variaveis de POST, Alterar somente se necessário 
+        //====================================================
+        $nome = $_POST['nome'];
+        $email = $_POST['email'];
+        $telefone = $_POST['telefone'];
+        $mensagem = $_POST['mensagem'];
+        //====================================================
+ 
+ 
+        //REMETENTE --> ESTE EMAIL TEM QUE SER VALIDO DO DOMINIO
+        //====================================================
+        $email_remetente = "contato@deborareinaldo.com.br"; // deve ser um email do dominio
+        //====================================================
+ 
+ 
+        //Configurações do email, ajustar conforme necessidade
+        //====================================================
+        $email_destinatario = "deborareinaldo.noivas@gmail.com"; // qualquer email pode receber os dados
+        $email_reply = "$email";
+        $email_assunto = "E-mail do site";
+        //====================================================
+ 
+ 
+        //Monta o Corpo da Mensagem
+        //====================================================
+        $email_conteudo = "Nome: $nome \n";
+        $email_conteudo .= "Email: $email \n";
+        $email_conteudo .=  "Telefone: $telefone \n";
+        $email_conteudo .=  "Mensagem: $mensagem \n";
+        //====================================================
+ 
+ 
+        //Seta os Headers (Alerar somente caso necessario)
+        //====================================================
+        $email_headers = implode ( "\n",array ( "From: $email_remetente", "Reply-To: $email_reply", "Subject: $email_assunto","Return-Path:  $email_remetente","MIME-Version: 1.0","X-Priority: 3","Content-Type: text/html; charset=UTF-8" ) );
+        //====================================================
+ 
+ 
+        //Enviando o email
+        //====================================================
+        if (mail ($email_destinatario, $email_assunto, nl2br($email_conteudo), $email_headers)){
+                echo "</b>E-Mail enviado com sucesso!</b>";
+        }
+        else{
+                echo "</b>Falha no envio do E-Mail!</b>";
+        }
+        //====================================================
 }
-/* Nas linhas acima foi feita uma confirmação do preenchimento dos formulários.. sendo que todos os
-campos preenchidos do formulário será enviado com a instrução mail() */
 ?>
